ad8c788910c9f99662b8ec2d14d292c1d3abced1
AnvayD <anvayabhi@gmail.com>
2026-01-26 14:02:35 -0500

    Config changess

--- app/backend/config.py
+++ app/backend/config.py
@@ -1,12 +1,12 @@
 # app/config.py
 import os
 
-OPENROUTER_API_KEY = "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"
+OPENROUTER_API_KEY = "Key"
 
 
 OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
-OPENROUTER_MODEL = "nvidia/nemotron-3-nano-30b-a3b:free"
+OPENROUTER_MODEL = "Model"
 
-QDRANT_URL = "http://localhost:6333"
+QDRANT_URL = "VectorDB"
 
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
