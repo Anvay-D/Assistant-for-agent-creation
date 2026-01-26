6bf00d0dca66d1b5a74ce31201bdb24acdc09a3e
AnvayD <anvayabhi@gmail.com>
2026-01-26 12:58:49 -0500

    Ingestion pipline completed

--- app/backend/config.py
+++ app/backend/config.py
@@ -1,12 +1,12 @@
 # app/config.py
 import os
 
-OPENROUTER_API_KEY = "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"
+OPENROUTER_API_KEY = "Key"
 
 
 OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
-OPENROUTER_MODEL = "xiaomi/mimo-v2-flash:free"
+OPENROUTER_MODEL = "Model"
 
-QDRANT_URL = "http://localhost:6333"
+QDRANT_URL = "VectorDB"
 
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
