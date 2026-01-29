f8520f2c6b481fdb03f7b438d8421a066152f7cb
AnvayD <anvayabhi@gmail.com>
2026-01-29 13:04:20 -0500

    Langgraph prompt added with contants improvement

--- app/backend/rag/rag_retiever.py
+++ app/backend/rag/rag_retiever.py
@@ -1,8 +1,10 @@
 # retriever_qdrant.py
 from qdrant_client import QdrantClient
 from sentence_transformers import SentenceTransformer
+from Lang_chain_store import LANGCHAIN_COLLECTION ,LANGGRAPH_COLLECTION 
 
-COLLECTION = "langchain_docs"
+# Set the collection wanted to retrieve using the Constant
+COLLECTION = LANGGRAPH_COLLECTION
 
 client = QdrantClient("http://localhost:6333")
 embedder = SentenceTransformer("all-MiniLM-L6-v2")
